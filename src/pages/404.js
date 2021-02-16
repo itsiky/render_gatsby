8d163a4152c91a9821abc7f49b365cb7d2328d25
Itsik Yehuda <itsiky@gmail.com>
2021-02-16 07:12:31 +0200

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,6 +8,8 @@ const NotFoundPage = () => (
     <SEO title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Link to="/">Go back to the homepage</Link>
+
   </Layout>
 )
 
