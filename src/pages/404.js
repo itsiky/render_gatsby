41019284fd1d069711e500ebeb11cdd171a72ac6
Itsik Yehuda <itsiky@gmail.com>
2021-02-16 07:17:14 +0200

    commit

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,15 +2,18 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
+ 
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Link to="/">Go back to the homepage.</Link>
 
   </Layout>
 )
+
 
 export default NotFoundPage
